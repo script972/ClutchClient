bf68056d0021cca54ffa105bdcadfa239ef19dc9
Vladislav <Ostremskii>
2017-05-04 19:00:07 +0300

    doing tools bar

--- app/src/main/java/com/devas/bear/clutchclient/MainActivity.java
+++ app/src/main/java/com/devas/bear/clutchclient/MainActivity.java
@@ -1,11 +1,11 @@
 package com.devas.bear.clutchclient;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
 
     private Toolbar toolbar;
